95d3e3ea2788344aaef03ebfaf91c00ad085df39
Victor Lourenco <vlourenco.m@gmail.com>
2020-06-08 15:21:20 -0300

    JSON response

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('<h1>Minha lista de tarefas :)</h1>');
+})
+
+app.get('/json', (req, res) => {
+    res.json({title: 'Tarefa X', done: true});
 })
 
 app.listen(3000, () => {
