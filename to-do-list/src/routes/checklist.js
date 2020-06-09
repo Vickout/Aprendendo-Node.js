c83fe44c991f3037c3be3e751f6d8f3455b13c0f
Victor Lourenco <vlourenco.m@gmail.com>
2020-06-09 16:20:31 -0300

    Using POST

--- to-do-list/src/routes/checklist.js
+++ to-do-list/src/routes/checklist.js
@@ -2,9 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
-router. get('/', (req, res) => {
+router.get('/', (req, res) => {
     console.log('Check');
     res.send();
+})
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.status(200).send(req.body);
 })
 
 module.exports = router;
