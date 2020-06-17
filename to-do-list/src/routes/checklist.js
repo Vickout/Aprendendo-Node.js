882798897bf1e148be7316b2374f6a1e51c9ffe4
Victor Lourenco <vlourenco.m@gmail.com>
2020-06-16 21:54:41 -0300

    Listing data from model

--- to-do-list/src/routes/checklist.js
+++ to-do-list/src/routes/checklist.js
@@ -4,28 +4,36 @@ const router = express.Router();
 
 const Checklist = require('../models/checklist');
 
-router.get('/', (req, res) => {
-    console.log('Check');
-    res.send();
+router.get('/', async (req, res) => {
+    try{
+        let checklists = await Checklist.find({});
+        res.status(200).json(checklists);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 })
 
 router.post('/', async (req, res) => {
     let { name } = req.body
     
     try{
-        let checklist = await Checklist.create({ name })
+        let checklist = await Checklist.create({ name });
         res.status(200).json(checklist);
     } catch (error) {
         res.status(422).json(error);
     }
 })
 
-router.get('/:id', (req, res) => {
-    console.log(req.body);
-    res.send(`ID: ${req.params.id}`);
+router.get('/:id', async (req, res) => {
+    try{
+        let checklist = await Checklist.findById(req.params.id)
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
 })
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
     console.log(req.body);
     res.send(`Put ID: ${req.params.id}`);
 })
